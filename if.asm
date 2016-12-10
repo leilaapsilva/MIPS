908e94e847e35551261cdac87220d3930888293e
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-12-09 22:51:53 -0200

    Update if.asm
    Exemplo retirado do livro do Patterson e comentado por mim.

--- if.asm
+++ if.asm
@@ -6,3 +6,6 @@ exit: ...
 #bne testa se os valores de $8 e $9 são diferentes. Se forem, pula a próxima
 #instrução e vai para o label exit. Caso sejam iguais, continua para a próxima
 #instrução somando então os dois valores e armazenando-os em $10.
+
+
+#Exemplo retirado do livro do Patterson e comentado por mim.
