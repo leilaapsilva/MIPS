90a0f5eb91e2d064ff597c06db91d418bc359e56
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-12-09 22:44:37 -0200

    Create if.asm

---
+++ if.asm
@@ -0,0 +1,8 @@
+bne $8, $9, exit
+add $10, $8, $9
+
+exit: ...
+
+#bne testa se os valores de $8 e $9 são diferentes. Se forem, pula a próxima
+#instrução e vai para o label exit. Caso sejam iguais, continua para a próxima
+#instrução somando então os dois valores e armazenando-os em $10.
